d5ac05919158c97435686dc680baacc85a99701b
Lanzador <89643951+Lanzador@users.noreply.github.com>
2021-10-05 23:22:03 +0300

    Update CDNClient.cs

--- SteamKit2/SteamKit2/Steam/CDNClient.cs
+++ SteamKit2/SteamKit2/Steam/CDNClient.cs
@@ -359,7 +359,7 @@ namespace SteamKit2
 
             //manifestData = ZipUtil.Decompress( manifestData );
             
-            manifestData = File.ReadAllBytes("SK2.manifest");
+            var manifestData = File.ReadAllBytes("SK2.manifest");
 
             var depotManifest = new DepotManifest( manifestData );
 
